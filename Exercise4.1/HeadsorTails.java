baf2306f9afb8f088cc7cbdd64c8b5037e13c184
細谷和真 <hosoyakazuma@hosoyawashinnoMacBook-Air.local>
2020-08-06 23:24:49 +0900

    Victory message added

--- Exercise4.1/HeadsorTails.java
+++ Exercise4.1/HeadsorTails.java
@@ -1,20 +1,22 @@
 import static java.lang.System.*;
-
- public class HeadsorTails{
-     public static void main(String args[]){
-         int i;
-         int counts = 0;
-         out.println("Tossing coin...");
-         for(i=0;i<=2;i++){
-             int rand = new java.util.Random().nextInt(2);
-             out.print("Round"+(i+1)+":");
-             if(rand == 0){
-                 out.println("Heads"); counts++;
-             }
-             else{
-                 out.println("Tails");
+public class HeadsorTails{
+    public static void main(String args[]){
+        int i;
+        int counts = 0;
+        out.println("Tossing coin...");
+        for(i=0;i<=2;i++){
+            int rand = new java.util.Random().nextInt(2);
+            out.print("Round"+(i+1)+":");
+            if(rand == 0){
+                out.println("Heads"); counts++;
+            }
+            else{
+                out.println("Tails");
              }
          }
          out.println("Heads:"+counts+",Tails:"+(3-counts));
+
+         if(counts>(3-counts)) out.println("You won!");
+         else out.println("You lost!");
      }
  } 
